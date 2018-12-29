0a888828fd621846cb37e2d025d72b2bc749f060
chdharmtravelyaari <dharmpal.chaudhary@travelyaari.com>
2018-12-29 13:12:09 +0530

    Fourth commit

--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -1,8 +1,13 @@
-import {Component} from 'angular2/core'
+import {Component} from 'angular2/core';
+import {Video} from './videos';
 
 @Component({
     selector:'playlist',
     templateUrl:'app/ts/playlist.component.html',
     inputs:['video']
 })
-export class PlaylistComponent{}
+export class PlaylistComponent{
+    onSelect(vid:Video){
+        console.log(JSON.stringify(vid));
+    }
+}
